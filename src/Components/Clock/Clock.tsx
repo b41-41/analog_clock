d6a86f2fc4e7c8e4209876d738cb7ec46e3f84be
DABIN JEONG <41@b41.kr>
2022-10-23 20:01:30 +0900

    [Add] 툴팁 적용
    - 마우스가 시계 안으로 들어왔을 때 띄우기
    - 마우스 위치에 따라 툴팁 표시 위치 변경

--- src/Components/Clock/Clock.tsx
+++ src/Components/Clock/Clock.tsx
@@ -1,10 +1,14 @@
 import React, { useEffect } from 'react';
+import Tooltip from '../Commons/Tooltip/Tooltip';
 import * as S from './Clock.Styled';
 
 type Props = {};
 
 const Clock = (props: Props) => {
   const [currentTime, setCurrentTime] = React.useState(new Date());
+  const [isTooltipVisible, setIsTooltipVisible] = React.useState(false);
+  const [mousePosition, setMousePosition] = React.useState({ x: 0, y: 0 });
+  const clockRef = React.useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const tick = setTimeout(() => {
@@ -15,8 +19,39 @@ const Clock = (props: Props) => {
     };
   }, [currentTime]);
 
+  useEffect(() => {
+    const openTooltip = () => {
+      setIsTooltipVisible(true);
+    };
+    const closeTooltip = () => {
+      setIsTooltipVisible(false);
+    };
+    const moveTooltip = (event: MouseEvent) => {
+      const offsetWidth = clockRef.current?.offsetWidth || 0;
+
+      if (event.x > offsetWidth / 2 && event.y > offsetWidth / 2) {
+        setMousePosition({ x: event.x - 120, y: event.y - 40 });
+      } else if (event.x > offsetWidth / 2 && event.y < offsetWidth / 2) {
+        setMousePosition({ x: event.x - 120, y: event.y + 20 });
+      } else if (event.x < offsetWidth / 2 && event.y > offsetWidth / 2) {
+        setMousePosition({ x: event.x + 20, y: event.y - 40 });
+      } else {
+        setMousePosition({ x: event.x + 20, y: event.y + 20 });
+      }
+    };
+
+    clockRef.current?.addEventListener('mouseenter', openTooltip);
+    clockRef.current?.addEventListener('mouseleave', closeTooltip);
+    clockRef.current?.addEventListener('mousemove', moveTooltip);
+    return () => {
+      clockRef.current?.removeEventListener('mouseenter', openTooltip);
+      clockRef.current?.removeEventListener('mouseleave', closeTooltip);
+      clockRef.current?.removeEventListener('mousemove', moveTooltip);
+    };
+  });
+
   return (
-    <S.ClockWrapper>
+    <S.ClockWrapper ref={clockRef}>
       <S.Time12>12</S.Time12>
       <S.Time3>3</S.Time3>
       <S.Time6>6</S.Time6>
@@ -24,6 +59,11 @@ const Clock = (props: Props) => {
       <S.HourHand hours={currentTime.getHours()} />
       <S.MinuteHand minute={currentTime.getMinutes()} />
       <S.SecondHand seconds={currentTime.getSeconds()} />
+      <Tooltip
+        currentTime={currentTime}
+        visible={isTooltipVisible}
+        mousePosition={mousePosition}
+      />
     </S.ClockWrapper>
   );
 };
---
+++ src/Utils/index.ts
@@ -0,0 +1,1 @@
+export { default as doubleDigitNumber } from './doubleDigitNumber';
